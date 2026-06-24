28703214a2a8a3c80ef62dd0a0ef319b8a45a119
Damia Nigar <nigardamia6@gmail.com>
2026-06-24 13:58:47 +0500

    Assignment-2

---
+++ assignment-2.lua
@@ -0,0 +1,47 @@
+-- Welcome Message
+local playerName = "Ali"
+print("Welcome to the game, " .. playerName .. "!")
+
+-- Coin Counter
+local coins = 0
+coins = coins + 10
+print(coins)
+
+-- Player Health System
+local health = 100
+health = health - 30
+print(health)
+
+-- Player Info Display
+local name = "Alex"
+local age = 12
+local isAlive = true
+print(name)
+print(age)
+print(isAlive)
+
+-- Score System
+local score = 50
+score = score + 25
+print(score)
+
+-- Data Type Practice
+local myNumber = 42
+local myString = "Roblox Lua"
+local myBoolean = false
+print(myNumber)
+print(myString)
+print(myBoolean)
+
+-- Simple Game Stats
+local kills = 3
+local deaths = 1
+print("Kills: " .. kills)
+print("Deaths: " .. deaths)
+
+-- Bonus: Treasure Game
+local treasureCoins = 0
+treasureCoins = treasureCoins + 1
+treasureCoins = treasureCoins + 1
+treasureCoins = treasureCoins + 1
+print(treasureCoins)
